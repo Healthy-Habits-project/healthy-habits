c4ea2cb69e84c858a0615a722fbcc5b1f09f888b
Logan Gray <lhgray@lcmail.lcsc.edu>
2024-04-08 11:24:27 -0700

    Debug logs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,44 +26,53 @@ const Home: React.FC = () => {
 
   // Function to set card color based on count
   const setCardColor = (cardId: string, color: string) => {
-    console.log('DEBUG: setCardColor function called!');
+    console.log('DEBUG: setCardColor() - START');
     const card = document.getElementById(cardId);
     if (card) {
       card.style.backgroundColor = color;
     }
-    console.log('DEBUG: setCardColor - cardId:', cardId);
-    console.log('DEBUG: setCardColor - color:', color);
+    console.log('DEBUG: setCardColor() - cardId:', cardId);
+    console.log('DEBUG: setCardColor() - color:', color);
   };
 
   // useEffect 1
   useEffect(() => {
     // Retrieve the counts from local storage and update the state
-    console.log('DEBUG: useEffect 1 called!');
+    console.log('DEBUG: useEffect 1 - START');
     const storedPhysicalCount = localStorage.getItem('physicalHealthCheckedCount');
+    console.log("DEBUG: useEffect 1 - storedPhysicalCount:", storedPhysicalCount);
     if (storedPhysicalCount) {
+      console.log("DEBUG: useEffect 1 - setPhysicalHealthCheckedCount:", Number(storedPhysicalCount));
       setPhysicalHealthCheckedCount(Number(storedPhysicalCount));
     }
 
     const storedMentalCount = localStorage.getItem('mentalHealthCheckedCount');
+    console.log("DEBUG: useEffect 1 - storedMentalCount:", storedMentalCount);
     if (storedMentalCount) {
+      console.log("DEBUG: useEffect 1 - setMentalHealthCheckedCount:", Number(storedMentalCount));
       setMentalHealthCheckedCount(Number(storedMentalCount));
     }
 
     const storedNutritionCount = localStorage.getItem('nutritionCheckedCount');
+    console.log("DEBUG: useEffect 1 - storedNutritionCount:", storedNutritionCount);
     if (storedNutritionCount) {
+      console.log("DEBUG: useEffect 1 - setNutritionCheckedCount:", Number(storedNutritionCount));
       setNutritionCheckedCount(Number(storedNutritionCount));
     }
 
     const storedSleepCount = localStorage.getItem('sleepCheckedCount');
+    console.log("DEBUG: useEffect 1 - storedSleepCount:", storedSleepCount);
     if (storedSleepCount) {
+      console.log("DEBUG: useEffect 1 - setSleepCheckedCount:", Number(storedSleepCount));
       setSleepCheckedCount(Number(storedSleepCount));
     }
+    console.log('DEBUG: useEffect 1 - END');
   }, [setPhysicalHealthCheckedCount, setMentalHealthCheckedCount, setNutritionCheckedCount, setSleepCheckedCount]);
 
   // useEffect 2
   useEffect(() => {
     // Update the card colors based on the retrieved counts
-    console.log('DEBUG: useEffect 2 called!');
+    console.log('DEBUG: useEffect 2 - START');
     const totalPhysicalCheckboxes = 6;
     const totalMentalCheckboxes = 8;
     const totalNutritionCheckboxes = 4;
@@ -74,10 +83,10 @@ const Home: React.FC = () => {
     const nutritionColor = getColorBasedOnCount(nutritionCheckedCount, totalNutritionCheckboxes);
     const sleepColor = getColorBasedOnCount(sleepCheckedCount, totalSleepCheckboxes);
 
-    console.log('physicalColor:', physicalColor);
-    console.log('mentalColor:', mentalColor);
-    console.log('nutritionColor:', nutritionColor);
-    console.log('sleepColor:', sleepColor);
+    console.log('DEBUG: useEffect 2 - physicalColor:', physicalColor);
+    console.log('DEBUG: useEffect 2 - mentalColor:', mentalColor);
+    console.log('DEBUG: useEffect 2 - nutritionColor:', nutritionColor);
+    console.log('DEBUG: useEffect 2 - sleepColor:', sleepColor);
 
     setCardColor("mentalCard", mentalColor);
     setCardColor("physicalCard", physicalColor);
@@ -94,11 +103,13 @@ const Home: React.FC = () => {
   // useEffect 3
   useEffect(() => {
     // Retrieve the username from localStorage when the component mounts
-    console.log('DEBUG: useEffect 3 called!');
+    console.log('DEBUG: useEffect 3 - START');
     const storedUserName = localStorage.getItem('userName');
+    console.log("DEBUG: useEffect 3 - storedUserName:", storedUserName);
     if (storedUserName) {
       setUserName(storedUserName); // Update the userName in your context
     }
+    console.log('DEBUG: useEffect 3 - END');
   }, [setUserName]);
 
   return (
